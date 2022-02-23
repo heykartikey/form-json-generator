3bf2968148da15440d7f4212c70f8e874a826234
hakpk5 <noreply1824@gmail.com>
2022-02-23 12:13:21 +0530

    merge conflict

--- src/context/JsonReducer.js
+++ src/context/JsonReducer.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { updateCommon } from "../utils/common/updateCommon";
 import { updateFieldId } from "../utils/common/updateFieldId";
 
@@ -60,59 +59,3 @@ const reducer = (state, action) => {
 };
 
 export default reducer;
-=======
-import { updateCommon } from "../utils/common/updateCommon";
-import { updateFieldId } from "../utils/common/updateFieldId";
-
-import { updateAlignment } from "../utils/alignment/updateAlignment";
-
-import { updateValidation } from "../utils/validation/updateValidation";
-import { updateLengthValidation } from "../utils/validation/updateLengthValidation";
-
-import { setCurrentField } from "../utils/setCurrentField";
-import { addField } from "../utils/addField";
-import { deleteField } from "../utils/deleteField";
-
-import { addPage } from "../utils/addPage";
-import { deletePage } from "../utils/deletePage";
-import { setCurrentPage } from "../utils/setCurrentPage";
-
-import {updatePageTitle} from '../utils/page/updateTitle';
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "UPDATE_COMMON":
-      return updateCommon(state, action);
-    case "UPDATE_FIELD_ID":
-      return updateFieldId(state, action);
-
-    case "UPDATE_ALIGNMENT":
-      return updateAlignment(state, action);
-
-    case "UPDATE_VALIDATION":
-      return updateValidation(state, action);
-    case "UPDATE_LENGTH_VALIDATION":
-      return updateLengthValidation(state, action);
-
-    case "SET_CURRENT_FIELD":
-      return setCurrentField(state, action);
-    case "ADD_FIELD":
-      return addField(state);
-    case "DELETE_FIELD":
-      return deleteField(state, action);
-
-    case "SET_CURRENT_PAGE":
-      return setCurrentPage(state, action);
-    case "ADD_PAGE":
-      return addPage(state);
-    case "DELETE_PAGE":
-      return deletePage(state, action);
-    case "UPDATE_PAGE_TITLE": 
-      return updatePageTitle(state, action);
-    default:
-      return state;
-  }
-};
-
-export default reducer;
->>>>>>> Stashed changes
