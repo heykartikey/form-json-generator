3ca490c2a6b90004f1af0ede3c642c90edb92171
Kartikey Sahu <71600854+kartikey-sahu@users.noreply.github.com>
2022-02-25 02:04:13 +0530

    Added Event Types

--- src/components/modals/events/EventActionModal.jsx
+++ src/components/modals/events/EventActionModal.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, forwardRef, useRef, useEffect } from "react";
 import {
   Button,
   Dialog,
@@ -11,10 +11,29 @@ import {
   Stack,
   TextField,
   Typography,
+  Divider,
 } from "@mui/material";
 
 import { JsonContext } from "../../../context/JsonContext";
-import { Close } from "@mui/icons-material";
+import { Add, Close } from "@mui/icons-material";
+
+const eventTypes = ["onSelect", "onClick", "onSubmit", "onEntryLimitReach"];
+
+const EventTypes = ({ eventType, setEventType }) => (
+  <TextField
+    select
+    size="small"
+    label="Event Types"
+    value={eventType}
+    onChange={(event) => setEventType(event.target.value)}
+  >
+    {eventTypes.map((eventType) => (
+      <MenuItem key={eventType} value={eventType}>
+        {eventType}
+      </MenuItem>
+    ))}
+  </TextField>
+);
 
 export default function EventActionsModal({
   open,
@@ -25,8 +44,18 @@ export default function EventActionsModal({
   const { events = [] } =
     state.pages[state.currentPage].fields.find(
       (field) => field.fieldId === fieldId
-    ) ?? [];
-  console.log("Events: ", events);
+    ) ?? {};
+
+  const [eventType, setEventType] = useState(events[0]?.type ?? "onSelect");
+
+  const actions = useRef([]);
+
+  useEffect(() => {
+    actions.current = events?.find(
+      (event) => event.type === eventType
+    )?.actions;
+  }, [eventType]);
+
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle id="scroll-dialog-title">
@@ -35,30 +64,44 @@ export default function EventActionsModal({
           justifyContent="space-between"
           alignItems="center"
         >
-          Actions
+          Events
           <IconButton onClick={() => handleClose(null)}>
             <Close />
           </IconButton>
         </Stack>
       </DialogTitle>
       <DialogContent dividers>
-        <DialogContentText id="scroll-dialog-description">
-          <TextField
-            fullWidth
-            select
-            size="small"
-            label="Event type"
-            value={events[0].type}
-            // onChange={updateHorizontal}
-          >
-            {events.map((event, index) => (
-              <MenuItem key={event.type} value={event.type}>
-                {event.type}
-              </MenuItem>
-            ))}
-          </TextField>
+        <DialogContentText>
+          <Stack gap={2}>
+            <Stack gap={2} direction="row" justifyContent="space-between">
+              <EventTypes eventType={eventType} setEventType={setEventType} />
+              <Button startIcon={<Add />} variant="outlined" size="small">
+                Add Action
+              </Button>
+            </Stack>
+          </Stack>
         </DialogContentText>
+        {actions.current.length > 0 ? (
+          <div>
+            {actions.current.map((action) => {
+              // TODO: return Action-specific component
+              return null
+            })}
+          </div>
+        ) : (
+          <DialogContentText>
+            <Typography
+              sx={{ textAlign: "center", mt: 2, fontStyle: "italic" }}
+            >
+              No actions added yet.
+            </Typography>
+          </DialogContentText>
+        )}
       </DialogContent>
+      <DialogActions>
+        <Button>Cancel</Button>
+        <Button variant="contained">Ok</Button>
+      </DialogActions>
     </Dialog>
   );
 }
